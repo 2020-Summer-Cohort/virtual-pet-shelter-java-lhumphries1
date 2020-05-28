7606d1801804a94efb51a7c267c4d6630c720700
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-05-28 14:54:20 -0400

    5-28-2020

--- src/main/java/shelter/VirtualPetShelterApp.java
+++ src/main/java/shelter/VirtualPetShelterApp.java
@@ -1,28 +1,40 @@
 package shelter;
 
-import java.util.Collection;
 import java.util.Scanner;
 
 public class VirtualPetShelterApp {
 
+
     static void main(String[] args) {
         System.out.println("Welcome to the Pet Shelter");
-        System.out.println("Choose your pet to take home!");
+        System.out.println("Would you like to visit a pet");
 
     }
 
-    public void gameLoop() {
+    public static void whatToDo(String petName){
+        System.out.println("Choose your interaction with you pet!");
+        System.out.println("1. Play with " + petName + ". ");
+        System.out.println("2. Give " + petName + " a drink.");
+        System.out.println("3. Feed " + petName + "!");
+        System.out.println("4. Adopt pet" + petName + ".");
+    }
+
+    public void gameLoop(Scanner input, String petName) {
         String choice;
         Scanner input = new Scanner(System.in);
         VirtualPetShelter virtualPetShelter = new VirtualPetShelter();
+        VirtualPet virtualPet = new VirtualPet("");
+
 
         while (true) {
             printPetStatus(virtualPetShelter);
-            System.out.println("Play with pets." + virtualPetShelter.petShelter);
+            System.out.println("");
 
-
-
+            System.out.println("Play with" + virtualPet.getName() + virtualPet.getBoredomNeed());
+            System.out.println("Give water to" + virtualPet.getName() + virtualPet.getThirstNeed());
+            System.out.println("Feed" + virtualPet.getName() + virtualPet.getHungerNeed());
             int userChoice = input.nextInt();
+
             if (userChoice == 1) {
                 System.out.println("Play with pet friends.");
                 printPetStatus(virtualPetShelter);
@@ -36,21 +48,8 @@ public class VirtualPetShelterApp {
                     continue;
                 }
             }
-            if (userChoice == 2) {
-                System.out.println("Would you like to adopt a pet.");
-                printPetStatus(virtualPetShelter);
-                choice = input.nextLine();
-                if (virtualPetShelter.petShelter.containsKey(choice)) {
-                    VirtualPet adoptPet = virtualPetShelter.petShelter.get(choice);
-                    System.out.println("You picked " + adoptPet.getName());
-                    virtualPetShelter.petShelter.remove(adoptPet);
-                } else {
-                    System.out.println("Iam sorry there no pet with that name");
-                    continue;
-                }
-            }
 
-            if (userChoice == 3) {
+            if (userChoice == 2) {
                 System.out.println("Would you like to give pets a drink");
                 printPetStatus(virtualPetShelter);
                 choice = input.nextLine();
@@ -63,7 +62,7 @@ public class VirtualPetShelterApp {
                     continue;
                 }
             }
-            if (userChoice == 4) {
+            if (userChoice == 3) {
                 System.out.println("Would you like to feed the pets?");
                 printPetStatus(virtualPetShelter);
                 choice = input.nextLine();
@@ -73,13 +72,23 @@ public class VirtualPetShelterApp {
                     feedPets.feed();
                 } else {
                     System.out.println("Iam sorry there are not pet with that name.");
+                    continue;
+                }
+            }
+            if (userChoice == 4) {
+                System.out.println("Would you like to adopt a pet.");
+                printPetStatus(virtualPetShelter);
+                choice = input.nextLine();
+                if (virtualPetShelter.petShelter.containsKey(choice)) {
+                    VirtualPet adoptPet = virtualPetShelter.petShelter.get(choice);
+                    System.out.println("You picked " + adoptPet.getName());
+                    virtualPetShelter.petShelter.remove(adoptPet);
+                } else {
+                    System.out.println("Iam sorry there no pet with that name");
                     break;
                 }
             }
-
         }
-
-
     }
 
 
